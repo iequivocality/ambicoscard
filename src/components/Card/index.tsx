08669a2f548bd6772f7e8d501496d7c0af8971cd
iequivocality <xanthiandere@gmail.com>
2025-06-18 00:06:39 +0800

    change css

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,10 +6,9 @@ import {
   type MouseEvent,
   type TouchEvent,
 } from "react";
-import { clamp, round } from "../../lib/Math";
+import { adjust, clamp, round } from "../../lib/Math";
 
-const springInteractSettings = { stiffness: 0.066, damping: 0.25 };
-const springPopoverSettings = { stiffness: 0.033, damping: 0.45 };
+const springInteractSettings = { stiffness: 66, damping: 5 };
 
 export function Card() {
   const [interacting, setInteracting] = useState(false);
@@ -19,20 +18,25 @@ export function Card() {
 
   const springRotateX = useSpring(0, springInteractSettings);
   const springRotateY = useSpring(0, springInteractSettings);
-  const springRotateDX = useSpring(0, springPopoverSettings);
-  const springRotateDY = useSpring(0, springPopoverSettings);
+
+  const springBackgroundX = useSpring(50, springInteractSettings);
+  const springBackgroundY = useSpring(50, springInteractSettings);
 
   const updateSprings = (
+    background: { x: number; y: number },
     rotate: { x: number; y: number },
     glare: { x: number; y: number; opacity: number },
   ) => {
+    springBackgroundX.set(background.x);
+    springBackgroundY.set(background.y);
+
     springRotateX.set(rotate.x);
     springRotateY.set(rotate.y);
 
     springGlareX.set(glare.x);
     springGlareY.set(glare.y);
     springGlareOpacity.set(glare.opacity);
-    console.log(glare);
+    console.log(glare.opacity);
   };
 
   const interact = (e: MouseEvent<HTMLButtonElement>) => {
@@ -55,32 +59,9 @@ export function Card() {
 
     updateSprings(
       {
-        x: round(-(center.x / 3.5)),
-        y: round(center.y / 2),
+        x: adjust(percent.x, 0, 100, 37, 63),
+        y: adjust(percent.y, 0, 100, 33, 67),
       },
-      { x: round(percent.x), y: round(percent.y), opacity: 1 },
-    );
-  };
-
-  const interactTouchMove = (e: TouchEvent) => {
-    const target = e.target as HTMLButtonElement;
-    const rect = target.getBoundingClientRect();
-    const absolute = {
-      x: e.touches[0].clientX - rect.left, // get mouse position from left
-      y: e.touches[0].clientY - rect.top, // get mouse position from right
-    };
-
-    const percent = {
-      x: clamp(round((100 / rect.width) * absolute.x)),
-      y: clamp(round((100 / rect.height) * absolute.y)),
-    };
-
-    const center = {
-      x: percent.x - 50,
-      y: percent.y - 50,
-    };
-
-    updateSprings(
       {
         x: round(-(center.x / 3.5)),
         y: round(center.y / 2),
@@ -89,24 +70,62 @@ export function Card() {
     );
   };
 
+  // const interactTouchMove = (e: TouchEvent) => {
+  //   const target = e.target as HTMLButtonElement;
+  //   const rect = target.getBoundingClientRect();
+  //   const absolute = {
+  //     x: e.touches[0].clientX - rect.left, // get mouse position from left
+  //     y: e.touches[0].clientY - rect.top, // get mouse position from right
+  //   };
+
+  //   const percent = {
+  //     x: clamp(round((100 / rect.width) * absolute.x)),
+  //     y: clamp(round((100 / rect.height) * absolute.y)),
+  //   };
+
+  //   const center = {
+  //     x: percent.x - 50,
+  //     y: percent.y - 50,
+  //   };
+
+  //   updateSprings(
+  //     {
+  //       x: adjust(percent.x, 0, 100, 37, 63),
+  //       y: adjust(percent.y, 0, 100, 33, 67),
+  //     },
+  //     {
+  //       x: round(-(center.x / 3.5)),
+  //       y: round(center.y / 2),
+  //     },
+  //     { x: round(percent.x), y: round(percent.y), opacity: 1 },
+  //   );
+  // };
+
   const interactEnd = (e: MouseEvent) => {
-    setTimeout(function () {
-      setInteracting(false);
+    if (!interacting) {
+      // springRotateX.jump(0);
+      // springRotateY.jump(0);
 
-      // springRotate.stiffness = snapStiff;
-      // springRotate.damping = snapDamp;
-      // springRotate.set({ x: 0, y: 0 }, { soft: 1 });
-      springRotateX.set(0);
-      springRotateY.set(0);
+      // springGlareX.jump(50);
+      // springGlareY.jump(50);
+      // springGlareOpacity.jump(0);
 
-      springGlareX.set(50);
-      springGlareY.set(50);
-      springGlareOpacity.set(0);
+      // springBackgroundX.jump(50);
+      // springBackgroundY.jump(50);
+      return;
+    }
 
-      // springBackground.stiffness = snapStiff;
-      // springBackground.damping = snapDamp;
-      // springBackground.set({ x: 50, y: 50 }, { soft: 1 });
-    }, 500);
+    setInteracting(false);
+
+    springRotateX.jump(0);
+    springRotateY.jump(0);
+
+    springGlareX.jump(50);
+    springGlareY.jump(50);
+    springGlareOpacity.jump(0);
+
+    springBackgroundX.jump(50);
+    springBackgroundY.jump(50);
   };
 
   const pointerFromCenter = useTransform(() =>
@@ -121,22 +140,28 @@ export function Card() {
   );
   const pointerFromTop = useTransform(() => springGlareY.get() / 100);
   const pointerFromLeft = useTransform(() => springGlareX.get() / 100);
-  const rotateX = useTransform(() => `${springRotateX.get() + springRotateDX.get()}deg`);
-  const rotateY = useTransform(() => `${springRotateY.get() + springRotateDY.get()}deg`);
+  const rotateX = useTransform(() => `${springRotateX.get()}deg`);
+  const rotateY = useTransform(() => `${springRotateY.get()}deg`);
+  const backgroundX = useTransform(() => `${springBackgroundX.get()}%`);
+  const backgroundY = useTransform(() => `${springBackgroundY.get()}%`);
+  const springGlareXPer = useTransform(() => `${springGlareX.get()}%`);
+  const springGlareYPer = useTransform(() => `${springGlareY.get()}%`);
 
   return (
     <motion.div
       className="card"
       style={
         {
-          "--pointer-x": springGlareX,
-          "--pointer-y": springGlareY,
+          "--pointer-x": springGlareXPer,
+          "--pointer-y": springGlareYPer,
           "--pointer-from-center": pointerFromCenter,
           "--pointer-from-top": pointerFromTop,
           "--pointer-from-left": pointerFromLeft,
           "--card-opacity": springGlareOpacity,
           "--rotate-x": rotateX,
           "--rotate-y": rotateY,
+          "--background-x": backgroundX,
+          "--background-y": backgroundY,
         } as CSSProperties
       }
     >
